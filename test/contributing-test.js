8eb6ff07b9cdaff8dbbf06a1dd9684fddfc6c709
Fedor Indutny <fedor@indutny.com>
2014-12-08 00:20:01 +0300

    contributing: finish parser

--- test/contributing-test.js
+++ test/contributing-test.js
@@ -22,6 +22,53 @@ function fn2text(fn) {
   }).join('\n');
 }
 
+var questions = fn2text(function() {/*
+  ## Irrelevant info
+
+  Yes
+
+  ### Caine's requirements
+
+  Hello! I am pleased to see your valuable contributing to this project.
+  Would you please mind answering a couple of questions to help me
+  classify this submission and/or gather required information for the
+  core team members?
+
+  #### Questions:
+
+  * _Issue-only_ Does this issue happen in core, or in some user-space
+    module from npm or other source? Please ensure that the test case
+    that reproduces this problem is not using any external dependencies.
+    If the error is not reproducible with just core modules - it is most
+    likely not a io.js problem.
+  * Which part of core do you think it might be related to?
+    _One of: `tls, crypto, buffer, http, https, assert, util, streams,
+    other`_
+  * Which versions of io.js do you think are affected by this?
+    _One of: `v0.10, v0.12, v1.0.0`_
+  * _PR-only_ Does `make test` pass after applying this Pull Request
+  * _PR-only_ Is the commit message properly formatted? (See
+    CONTRIBUTING.md for more information)
+
+  Please provide the answers in an ordered list like this:
+
+  1. Answer for the first question
+  2. Answer for the second question
+  3. ...
+
+  Note that I am just a bot with a limited human-reply parsing abilities,
+  so please be very careful with numbers and don't skip the questions!
+
+  The core team members will be summoned here right after your answers!
+
+  Truly yours,
+  Caine.
+
+  ### Some non-caine's stuff
+
+  Yep.
+*/});
+
 describe('Contributing', function() {
   describe('.renderMd()', function() {
     function test(fn) {
@@ -69,49 +116,10 @@ describe('Contributing', function() {
 
   describe('.parse()', function() {
     it('should parse semantic markdown', function() {
-      var out = contributing.parse(fn2text(function() {/*
-        ## Irrelevant info
-
-        Yes
-
-        ### Caine's requirements
-
-        Hello! I am pleased to see your valuable contributing to this project.
-        Would you please mind answering a couple of questions to help me
-        classify this submission and/or gather required information for the
-        core team members?
-
-        #### Questions:
-
-        * _Issue-only_ Does this issue happen in core, or in some user-space
-          module from npm or other source? Please ensure that the test case
-          that reproduces this problem is not using any external dependencies.
-          If the error is not reproducible with just core modules - it is most
-          likely not a io.js problem.
-        * Which part of core do you think it might be related to?
-        * Which versions of io.js do you think are affected by this?
-        * _PR-only_ Does `make test` pass after applying this Pull Request
-        * _PR-only_ Is the commit message properly formatted? (See
-          CONTRIBUTING.md for more information)
-
-        Please provide the answers in an ordered list like this:
-
-        1. Answer for the first question
-        2. Answer for the second question
-        3. ...
-
-        Note that I am just a bot with a limited human-reply parsing abilities,
-        so please be very careful with numbers and don't skip the questions!
-
-        The core team members will be summoned here right after your answers!
-
-        Truly yours,
-        Caine.
-
-        ### Some non-caine's stuff
-
-        Yep.
-      */}));
+      var out = contributing.parse(questions);
+      assert(typeof out === 'object');
+      assert(out.text);
+      assert.equal(out.questions.length, 5);
     });
   });
 });
