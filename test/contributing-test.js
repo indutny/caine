1cc38071a28752b54e6c5e6bd8b5a46fe530dfb5
Fedor Indutny <fedor@indutny.com>
2014-12-08 01:39:20 +0300

    lib: save progress

--- test/contributing-test.js
+++ test/contributing-test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var marked = require('marked');
+var fs = require('fs');
 
 var caine = require('../');
 var contributing = caine.contributing;
@@ -22,54 +23,8 @@ function fn2text(fn) {
   }).join('\n');
 }
 
-var questions = fn2text(function() {/*
-  ## Irrelevant info
-
-  Yes
-
-  ### Caine's requirements
-
-  Hello! I am pleased to see your valuable contributing to this project.
-  Would you please mind answering a couple of questions to help me
-  classify this submission and/or gather required information for the
-  core team members?
-
-  #### Questions:
-
-  * _Issue-only_ Does this issue happen in core, or in some user-space
-    module from npm or other source? Please ensure that the test case
-    that reproduces this problem is not using any external dependencies.
-    If the error is not reproducible with just core modules - it is most
-    likely not a io.js problem. _Expected: `yes`_
-  * Which part of core do you think it might be related to?
-    _One of: `tls, crypto, buffer, http, https, assert, util, streams,
-    other`_
-  * Which versions of io.js do you think are affected by this?
-    _One of: `v0.10, v0.12, v1.0.0`_
-  * _PR-only_ Does `make test` pass after applying this Pull Request.
-    _Expected: `yes`_
-  * _PR-only_ Is the commit message properly formatted? (See
-    CONTRIBUTING.md for more information)
-    _Expected: `yes`_
-
-  Please provide the answers in an ordered list like this:
-
-  1. Answer for the first question
-  2. Answer for the second question
-  3. ...
-
-  Note that I am just a bot with a limited human-reply parsing abilities,
-  so please be very careful with numbers and don't skip the questions!
-
-  The core team members will be summoned here right after your answers!
-
-  Truly yours,
-  Caine.
-
-  ### Some non-caine's stuff
-
-  Yep.
-*/});
+var questions = fs.readFileSync(__dirname + '/fixtures/contributing.md')
+                  .toString();
 
 describe('Contributing', function() {
   describe('.renderMd()', function() {
